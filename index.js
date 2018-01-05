2e083617178e1be55e9afd1ac12fb50eebafd58d
Harish <Harish.inflexiontech@gmail.com>
2018-01-05 16:06:00 +0530

    Created a project and folder structure

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import { HomeScreen } from './src/screens/Home';
+
+AppRegistry.registerComponent('silver_needle_dashboard', () => HomeScreen);
---
+++ src/styles/cssHome.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const cssHome = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    imgReactLog: {
+        width: 50,
+        height: 50,
+    }
+})
